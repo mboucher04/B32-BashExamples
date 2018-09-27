78f7843c9c0385567096c6c39d0d2b5448536f35
Melanie Boucher <mel.boucher04@gmail.com>
2018-09-27 14:55:46 -0400

    les boucles

--- 05-boucles.sh
+++ 05-boucles.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
 
+for ((i=0;i<5;i++))
+do
+	echo $i
+done
+
+liste=`ls` #Accent grave permet d'executer une commande  # si on met /tmp apres `ls` liste elem de tmp
+for element in $liste 
+do
+	echo $element
+done
+
+user=`whoami`
+echo "Salut $user"
+
+resultat=o
+
+while test $resultat = o;do		# -o = OR -a = AND (aprèes le resultat = o)
+	read -p "Entrez une lettre:" resultat
+done
