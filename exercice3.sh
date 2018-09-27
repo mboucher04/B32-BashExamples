77b48c7bde3537503103360af783f447a700b6d3
Melanie Boucher <mel.boucher04@gmail.com>
2018-09-27 16:50:05 -0400

    ... BOOM! qui bouge!

--- exercice3.sh
+++ exercice3.sh
@@ -1,1 +1,21 @@
 #!/bin/bash
+
+addSpace() {
+	for ((i=0;i<$1;i++))
+	do
+		echo -n " "
+	done
+}
+
+for ((i=0;i<15;i++))
+do 
+	addSpace $i
+	echo "..."
+	sleep 0.2
+	clear
+done
+
+addSpace 15
+echo "Boom!"
+sleep 2
+clear
