a8201a6f1731f1b0f1061646878a7ec7d51d10bf
Melanie Boucher <mel.boucher04@gmail.com>
2018-09-27 16:23:34 -0400

    Dictionnaire exercice 2

--- exercice2.sh
+++ exercice2.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+
+afficherMenu(){
+lettre=lettre
+mot=mot
+	while test $lettre != q;do
+		echo "----------------------------"
+		echo "d = Dictionnaire" 
+		echo "q = Quitter" 
+		echo "---------------------------"
+		read -p "Entrez une lettre: " lettre
+		if test $lettre = d;then
+			read -p "Entrez un mot: " mot
+			./exercice1.sh $mot
+			read x
+		fi
+	done
+}
+afficherMenu
