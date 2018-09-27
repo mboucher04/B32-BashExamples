0f795787f85ceea089fa119e6fc393f080d3e822
Melanie Boucher <mel.boucher04@gmail.com>
2018-09-27 15:21:49 -0400

    Done with functions

--- 07-fonctions.sh
+++ 07-fonctions.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 
+maFonction(){
+	echo "paramètre: $1"
+}
+
+maFonction allo
+
+afficherInfo() {
+	echo "--------------------"
+	echo "Kernel: "	`uname -rs`
+	echo "--------------------"
+}
+
+afficherInfo
