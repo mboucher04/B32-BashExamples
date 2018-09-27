d5dc42ec30b015f7b067f5cedd06445c0c50d551
Melanie Boucher <mel.boucher04@gmail.com>
2018-09-27 14:19:41 -0400

    Parametres bash

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
 
+# Assignation (sans $)
+prenom=David 
+
+# Lecture de la variable, toujours avec $
+echo $prenom
+
+declare -i age=33 # La variable est un entier 
+declare -r age2=34 # Variable en lecture seule, impossible de réaffecter
+
+cours="Cours de Linux" #Si la variable contient un espace, pas le choix de mettre "autour
+
+# Les variables systèmes: 
+# ./02-variables.sh fred test: on met les parametres en lancant le programme
+echo Prog $0 avec premier param $1, deuxième param $2, total est $# param
