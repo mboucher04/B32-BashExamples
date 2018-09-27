1b8780f1a7173f72983dabbb5346ab3a9d194735
Melanie Boucher <mel.boucher04@gmail.com>
2018-09-27 15:44:47 -0400

    Verifie si mot existe

--- exercice1.sh
+++ exercice1.sh
@@ -2,4 +2,10 @@
 
 # La ligne suivante permet de vérifier si un mot existe dans le dictionnaire
 # Il faut suffixer cet URL avec le mot entré par l'uasger (ex: word=chat)
-#wget -qO - http://dictionary.objectif8.com/exists.php?word=
+res=`wget -qO - http://dictionary.objectif8.com/exists.php?word=$1`
+
+if test $res -eq 1;then
+	echo "Existe"
+else
+	echo "N'existe pas"
+fi
